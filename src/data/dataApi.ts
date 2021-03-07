c467aa41dedba6994d5e2dd9f83915972d69192d
Paulo Henrique Queiroz <paulo.queiroz@bairesdev.com>
2021-03-07 00:19:24 -0300

    add real api endpoint

--- src/data/dataApi.ts
+++ src/data/dataApi.ts
@@ -1,6 +1,6 @@
 import { IRestaurant } from "../models/IRestaurant";
 
-const dataUrl = "/assets/data.json";
+const dataUrl = "https://api.sandbox.eatapp.co/public/v2/restaurants/14bf9273-64f3-4b39-875b-a616fc83f453";
 
  export const getRestautant = async () => {
   const response = await Promise.all([fetch(dataUrl)]);
